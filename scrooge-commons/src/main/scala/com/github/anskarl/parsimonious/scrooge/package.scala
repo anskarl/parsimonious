51ed7bca240243d4aeec65d06a52028eb57c4b04
Anastasios Skarlatidis <a.skarlatidis@gmail.com>
2022-08-09 13:53:57 +0300

    Pre-release updates (part3)
      - extension functions for scrooge jackson
      - minor refactorings

--- scrooge-commons/src/main/scala/com/github/anskarl/parsimonious/scrooge/package.scala
+++ scrooge-commons/src/main/scala/com/github/anskarl/parsimonious/scrooge/package.scala
@@ -1,15 +1,10 @@
 package com.github.anskarl.parsimonious
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.apache.thrift.protocol.{TCompactProtocol, TProtocolFactory}
-
 
 package object scrooge {
 
   type ThriftStructWithProduct = Class[_ <: com.twitter.scrooge.ThriftStruct with Product]
-
-  private[scrooge] final val mapper = new ObjectMapper().registerModule(DefaultScalaModule)
 
   case class ScroogeConfig(
     keyName: String = "key",
